569d6f7aa8297376e793ad827794ac9aef40c977
YuvrajKumar <92965840+RayDarklight@users.noreply.github.com>
2024-07-14 12:52:14 +0530

    dunno shit happened and resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
+import Footer from "./components/Footer"
 import Main from "./components/Main"
 import SideBar from "./components/SideBar"
 
 
 function App() {
-  
+
 
   return (
     <>
-      <SideBar></SideBar>
-      <Main/>
-      <Footer/>
-      
+      <SideBar />
+      <Main />
+      <Footer />
     </>
   )
 }
