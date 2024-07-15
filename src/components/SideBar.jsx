a25d8f2de96f6c9956e4a097af249066c5b5c001
YuvrajKumar <92965840+RayDarklight@users.noreply.github.com>
2024-07-15 18:26:55 +0530

    compleated

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,13 +1,13 @@
 export default function SideBar(props) {
-    const{handleToggleModal} = props
+  const { handleToggleModal, data } = props
   return (
-    <div  className="sidebar">
+    <div className="sidebar">
       <div onClick={handleToggleModal} className="bgOverlay"></div>
       <div className="sidebarContents">
-        <h2>The Brutual Martian Landscape</h2>
-        <div>
-          <p>Description</p>
-          <p> **Ancient Delta in Jezero Crater**:</p>
+        <h2>{data?.title}</h2>
+        <div className="descriptionContainer">
+          <p className="descriptionTitle">{data?.date}</p>
+          <p> {data?.explanation}</p>
         </div>
         <button onClick={handleToggleModal}>
           <i className="fa-solid fa-arrow-right"></i>
