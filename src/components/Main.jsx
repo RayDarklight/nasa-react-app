329868ecbecea63f656808e42b0c77347bc7f41f
YuvrajKumar <92965840+RayDarklight@users.noreply.github.com>
2024-07-15 18:53:36 +0530

    dl

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,5 @@
 export default function Main(props) {
-  const { data } = props;
+  const { data, handleToggleModal } = props;
   return (
     <div onClick={handleToggleModal} className="imageContainer">
       <img src={data?.hdurl} alt={data?.title || 'bg-image'} className="bgImage" />
