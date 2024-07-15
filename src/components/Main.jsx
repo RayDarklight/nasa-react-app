88393d5f160d339f586cfff0cacfe3bacda65f6c
YuvrajKumar <92965840+RayDarklight@users.noreply.github.com>
2024-07-15 18:50:36 +0530

    onclick on image itself

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 export default function Main(props) {
   const { data } = props;
   return (
-    <div className="imageContainer">
+    <div onClick={handleToggleModal} className="imageContainer">
       <img src={data?.hdurl} alt={data?.title || 'bg-image'} className="bgImage" />
     </div>
   );
